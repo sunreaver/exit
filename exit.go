535239c3f92321d36f0836ce987404132263f106
weit <weit@jumei.com>
2018-05-11 17:49:46 +0800

    1. 修改方法名 2. 3. 4.

--- exit.go
+++ exit.go
@@ -8,12 +8,12 @@ var (
 	once sync.Once
 )
 
-// RegisteExiter will Registe退出信号
+// RegistExiter will Registe退出信号
 // 当进程接到退出信号
 // 会写入exit信号
 // 并等待业务端delay信号的写入
 // 当接收到业务端delay信号，会执行os.Exit(0)
-func RegisteExiter(delay <-chan *Channel) (exit <-chan *Channel) {
+func RegistExiter(delay <-chan *Channel) (exit <-chan *Channel) {
 	once.Do(func() {
 		go notify()
 	})
