5f6f76592456a2850e5e62edc0b338f6c889f9c9
weit <weit@jumei.com>
2018-05-11 17:44:31 +0800

    1. 添加例子 2. 3. 4.

--- doc.go
+++ doc.go
@@ -1,3 +1,34 @@
-// Package exit 接收系统退出信号，并发送给注册的业务端，然后等待业务端处理完成后，执行os.Exit(0)
-//
+/*
+Package exit 接收系统退出信号，并发送给注册的业务端，然后等待业务端处理完成后，执行os.Exit(0)
+
+Example:
+
+	package main
+
+	import (
+		"fmt"
+
+		"github.com/sunreaver/exit"
+	)
+
+	func main() {
+		o := make(chan *exit.Channel, 1)
+		c := exit.RegisteExiter(o)
+
+		select {
+		case <-c:
+			fmt.Println("exit chan")
+		}
+
+		// do something
+		// example: flush buffer
+
+		close(o)
+
+		select {}
+
+		fmt.Println("ok")
+	}
+
+*/
 package exit
